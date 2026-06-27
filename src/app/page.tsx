b66690d7c36eb3abf0f9629ce94344f4768acfcb
winnerbanjo <winnerbanjochannel@gmail.com>
2026-06-27 14:10:46 +0100

    Force dynamic admin root

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import { RoleLoginPage } from "@/components/role-login-page";
 
+export const dynamic = "force-dynamic";
+
 export default function HomePage() {
   return <RoleLoginPage mode="admin" />;
 }
